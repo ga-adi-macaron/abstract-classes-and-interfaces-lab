2d154ecbc90d36043fb468a134e7158d2508ddd9
NikShuvalov <nshuvalov89@gmail.com>
2016-10-13 15:47:28 -0400

    Initial commit. Made Abstract remote class

---
+++ abstract-class-interfaces/src/NikitaS/Main.java
@@ -0,0 +1,55 @@
+package NikitaS;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+//
+//    Abstract Class
+//
+
+//        Interfaces
+//
+//        Create an interface class named WaterProof.
+//        Inside this class, declare a method isWaterProof. What do you think it should return?
+//
+//        Create an interface class named Rechargeable.
+//
+//        Inside this class, declare a method isRechargeable. What do you think it should return?
+//
+//        Create an interface class named Universal.
+//
+//        Inside this class, declare a method supportsBrands. It should return nothing.
+//        Concrete Class
+//
+//        Create three concrete classes that extends the abstract class Remote, name the concrete class one of your favorite brands. (Samsung, LG, Panasonic, Sony, etc.)
+//
+//        Implement the methods you declared in the Remote abstract class which are required to be implemented in each concrete class.
+//
+//        Make sure the implementations output something different for each concrete class.
+//
+//        Implement the WaterProof, Rechargeable, and Universal interfaces.
+//
+//        Implement the methods defined by those interfaces. You can choose what to do, but at the minimum print something out to the terminal.
+//
+//        Main Java Class
+//
+//        Create a List of Remote objects.
+//
+//        Populate the List of Remote objects with new instances of the concrete classes you created.
+//
+//        Create a loop that loops as many times as there are items in the List of Remote objects. (You choose the kind of loop)
+//
+//        Inside the loop call the numberButtonPressed method on each object and pass it the current iteration value of the loop.
+//
+//        Inside the loop call the channelUp, channelDown, volumeUp, volumeDown methods on each object.
+//
+//        Bonus:
+//
+//        Create 3 more concrete classes that extend Remote and use them in the loop.
+//        Deliverable
+//
+//        Pull request from forked Project on GitHub that contains your implementation of the Remote abstract class, the WaterProof, Rechargeable, and Universal interface classes, and the main program that uses the classes you created.
